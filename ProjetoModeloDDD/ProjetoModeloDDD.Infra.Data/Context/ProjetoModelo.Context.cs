be6b0629688e19048b79a65dc972c9ea962c38ae
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2026-02-05 15:39:43 -0300

    feat: adiciona entidade Cliente e configura EF com migration inicial

---
+++ ProjetoModeloDDD/ProjetoModeloDDD.Infra.Data/Context/ProjetoModelo.Context.cs
@@ -0,0 +1,15 @@
+﻿using ProjetoModeloDDD.Domain.Entities;
+using System.Data.Entity;
+
+namespace ProjetoModeloDDD.Infra.Data.Context
+{
+    public class ProjetoModelo : DbContext
+    {
+        public ProjetoModelo() : base("ProjetoModeloDDDConnection")
+		{
+            
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
