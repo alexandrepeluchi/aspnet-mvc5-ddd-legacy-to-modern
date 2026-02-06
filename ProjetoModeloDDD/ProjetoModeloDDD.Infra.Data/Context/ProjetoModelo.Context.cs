2feb3f666fdd81889090e03a10eb51878a03d8ae
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2026-02-06 13:34:41 -0300

    feat: adiciona configuracoes globais no DbContext e controle automatico de DataCadastro

--- ProjetoModeloDDD/ProjetoModeloDDD.Infra.Data/Context/ProjetoModelo.Context.cs
+++ ProjetoModeloDDD/ProjetoModeloDDD.Infra.Data/Context/ProjetoModelo.Context.cs
@@ -1,5 +1,7 @@
 ﻿using ProjetoModeloDDD.Domain.Entities;
+using System;
 using System.Data.Entity;
+using System.Linq;
 
 namespace ProjetoModeloDDD.Infra.Data.Context
 {
@@ -11,5 +13,39 @@ namespace ProjetoModeloDDD.Infra.Data.Context
         }
 
         public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<System.Data.Entity.ModelConfiguration.Conventions.PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<System.Data.Entity.ModelConfiguration.Conventions.OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<System.Data.Entity.ModelConfiguration.Conventions.ManyToManyCascadeDeleteConvention>();
+
+            modelBuilder.Properties()
+                .Where(p => p.Name == p.ReflectedType.Name + "Id")
+                .Configure(p => p.IsKey());
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar"));
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasMaxLength(100));
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(e => e.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
+            return base.SaveChanges();
+        }
     }
 }
