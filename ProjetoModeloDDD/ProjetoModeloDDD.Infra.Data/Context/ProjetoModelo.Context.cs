a1f00443904b61633ece9b3e3aae9ee015ac4bda
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2026-02-07 16:14:27 -0300

    chore: registra configuracao da entidade Produto no DbContext

--- ProjetoModeloDDD/ProjetoModeloDDD.Infra.Data/Context/ProjetoModelo.Context.cs
+++ ProjetoModeloDDD/ProjetoModeloDDD.Infra.Data/Context/ProjetoModelo.Context.cs
@@ -14,6 +14,7 @@ namespace ProjetoModeloDDD.Infra.Data.Context
         }
 
         public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Produto> Produtos{ get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
@@ -32,6 +33,7 @@ namespace ProjetoModeloDDD.Infra.Data.Context
                 .Configure(p => p.HasMaxLength(100));
 
             modelBuilder.Configurations.Add(new ClienteConfiguration());
+            modelBuilder.Configurations.Add(new ProdutoConfiguration());
         }
 
         public override int SaveChanges()
