5dbeba0bcf9bb889c6c024cf863328a668b977ee
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2026-03-03 17:39:08 -0300

    feat: implementa ProdutoRepository com metodo BuscarPorNome

---
+++ ProjetoModeloDDD/ProjetoModeloDDD.Infra.Data/Repositories/ProdutoRepository.cs
@@ -0,0 +1,16 @@
+﻿
+using ProjetoModeloDDD.Domain.Entities;
+using ProjetoModeloDDD.Domain.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjetoModeloDDD.Infra.Data.Repositories
+{
+    public class ProdutoRepository : RepositoryBase<Produto>, IProdutoRepository
+    {
+        public IEnumerable<Produto> BuscarPorNome(string nome)
+        {
+            return DbContext.Produtos.Where(p => p.Nome == nome).ToList();
+        }
+    }
+}
